a9a71bf3266a6815af5e5dc47d4bee07bc805050
alek.frohlich <alek.frohlich@gmail.com>
2020-03-07 20:59:19 -0300

    Add signal handlers

--- windows/main.py
+++ windows/main.py
@@ -1,30 +1,34 @@
 from gi.repository import Gtk
 
-# TODO:
-# Implement handlers
-
 class MainWindow:
     def __init__(self):
         self.builder = Gtk.Builder()
-        self.builder.add_from_file("layouts/release01.glade")
+        self.builder.add_from_file("glade/z_gui_layout.glade")
         self.builder.get_object("viewport").set_size_request(500,500)
         handlers = {
             "on_destroy" : Gtk.main_quit,
-            "on_draw": self.dummy,
-            "on_button_up_clicked": self.dummy,
-            "on_button_down_clicked":  self.dummy,
-            "on_button_left_clicked":  self.dummy,
-            "on_button_right_clicked": self.dummy,
-            "on_zoom_in":  self.dummy,
-            "on_zoom_out": self.dummy,
-            # menu bar buttons
+            "on_draw": self.fixme,
+            # Controls
+            "on_up_button": self.fixme,
+            "on_left_button":  self.fixme,
+            "on_right_button": self.fixme,
+            "on_down_button":  self.fixme,
+            "on_zoom_in":  self.fixme,
+            "on_zoom_out": self.fixme,
+            "on_x_button": self.fixme,
+            "on_y_button": self.fixme,
+            "on_z_button": self.fixme,
+            # Menu bar
             "on_menu_bar_quit": Gtk.main_quit,
-            "on_create_wireframe": self.dummy,
+            "on_create_wireframe": self._create_wireframe,
         }
         self.builder.connect_signals(handlers)
 
-    def dummy(self):
-        print("hello!")
+    def fixme(self, _):
+        print("Feature not yet implemented!")
 
     def show(self):
         self.builder.get_object("main_window").show_all()
+
+    def _create_wireframe(self, _):
+        print("Wireframe do favarin!")
