e84ab10819f9a4384355ee5829a7c60e01eb83d9
alek.frohlich <alek.frohlich@gmail.com>
2020-03-07 23:15:17 -0300

    Show/hide 'Create object' dialog

--- windows/main.py
+++ windows/main.py
@@ -1,3 +1,5 @@
+""""""
+
 from gi.repository import Gtk
 
 class MainWindow:
@@ -20,9 +22,14 @@ class MainWindow:
             "on_z_button": self.fixme,
             # Menu bar
             "on_menu_bar_quit": Gtk.main_quit,
-            "on_create_wireframe": self._create_wireframe,
+            "on_create_object": self._create_object,
+            # Create object dialog
+            "on_create_object_ok": self._dialog_ok,
+            "on_create_object_cancel": self._dialog_close,
+
         }
         self.builder.connect_signals(handlers)
+        self.dialog = None
 
     def fixme(self, _):
         print("Feature not yet implemented!")
@@ -30,5 +37,23 @@ class MainWindow:
     def show(self):
         self.builder.get_object("main_window").show_all()
 
-    def _create_wireframe(self, _):
-        print("Wireframe do favarin!")
+    def _create_object(self, _):
+        self.dialog = self.builder.get_object("create_object_dialog")
+        self.dialog.run()
+        self.dialog.hide()
+
+    def _dialog_ok(self, _):
+        # TODO:
+        # Read name from text entry and check if it already exists (if so, show error message on console)
+        # Read points from active page (and get object type)
+        # If line or wireframe, check for existence (same point / colinear?)
+        # If wireframe, check pattern
+        # Create object and add it to the world
+        # Add object to TreeStore and make it visible
+
+        if self.dialog:
+            self.dialog.response(Gtk.ResponseType.OK)
+
+    def _dialog_close(self, _):
+        if self.dialog:
+            self.dialog.response(Gtk.ResponseType.CLOSE)
