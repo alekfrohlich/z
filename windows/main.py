61408775291805247cf1e4bf535b01bbeda46ce4
alek.frohlich <alek.frohlich@gmail.com>
2020-03-08 09:59:57 -0300

    Reading form input and displaying back to terminal

--- windows/main.py
+++ windows/main.py
@@ -29,7 +29,7 @@ class MainWindow:
 
         }
         self.builder.connect_signals(handlers)
-        self.dialog = None
+        self.dialog = self.builder.get_object("create_object_dialog")
 
     def fixme(self, _):
         print("Feature not yet implemented!")
@@ -38,22 +38,58 @@ class MainWindow:
         self.builder.get_object("main_window").show_all()
 
     def _create_object(self, _):
-        self.dialog = self.builder.get_object("create_object_dialog")
-        self.dialog.run()
+        """ Display 'Create object' dialog and wait for it's response. """
+        self.dialog.run() # Log response type
         self.dialog.hide()
 
-    def _dialog_ok(self, _):
-        # TODO:
+    def _dialog_ok(self, _) -> bool:
+        """ Check if form input is valid. If so, create new object with it. """
         # Read name from text entry and check if it already exists (if so, show error message on console)
+        object_name = self.builder.get_object("name_dialog_field").get_text()
+        if object_name == "asd":
+            # Log to console
+            print("Object name: " + object_name + " already taken!")
+            return False
+
         # Read points from active page (and get object type)
-        # If line or wireframe, check for existence (same point / colinear?)
-        # If wireframe, check pattern
+        stack = self.builder.get_object("object_type_stack")
+        page = stack.get_visible_child()
+        page_name = page.get_name()
+
+        if page_name == "point_page":
+            x = int(self.builder.get_object("point_x").get_text())
+            y = int(self.builder.get_object("point_y").get_text())
+            z = int(self.builder.get_object("point_z").get_text())
+            print("Point object at " + str((x,y,z)))
+
+        elif page_name == "line_page":
+            x1 = int(self.builder.get_object("line_x1").get_text())
+            y1 = int(self.builder.get_object("line_y1").get_text())
+            z1 = int(self.builder.get_object("line_z1").get_text())
+            x2 = int(self.builder.get_object("line_x2").get_text())
+            y2 = int(self.builder.get_object("line_y2").get_text())
+            z2 = int(self.builder.get_object("line_z2").get_text())
+            print("Line object from " + str((x1,y1,z1)) + " to " + str((x2,y2,z2)))
+            # Verify if inside world and if not equal
+            if (x1,y1,z1) == (x2, y2, z2):
+                print("Two equal points cannot compose a line!")
+                return False
+
+        elif page_name == "wireframe_page":
+            wireframe_expression = self.builder.get_object("wireframe_expression").get_text()
+            print("Wireframe at " + wireframe_expression)
+            # Verify expression and check if colinear
+
+        else: # curves_page
+            print("Curves not yet implemented!")
+            return False
+
+        # TODO:
         # Create object and add it to the world
         # Add object to TreeStore and make it visible
 
-        if self.dialog:
-            self.dialog.response(Gtk.ResponseType.OK)
+        self.dialog.response(Gtk.ResponseType.OK)
 
     def _dialog_close(self, _):
-        if self.dialog:
-            self.dialog.response(Gtk.ResponseType.CLOSE)
+        """ Closes dialog without creating object. """
+        self.dialog.response(Gtk.ResponseType.CLOSE)
